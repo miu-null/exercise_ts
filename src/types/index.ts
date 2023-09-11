df1f66ca244b9c2230e7f943ae1d35a1de56bfbb
Hojin Kim <miu.null@gmail.com>
2023-09-11 13:11:39 +0900

    make types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export enum Color {
+    Red = "Red",
+    Green = "Green",
+    Blue = "Blue",
+}
+
+export enum Direction {
+    up = "Up",
+    down = "Down",
+    left = "Left",
+    right = "Right",
+}
+
+export interface thingsInLife {
+    colorOfChair: Color;
+    arrow: Direction;
+}
