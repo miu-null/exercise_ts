e30119582774291d03f32b9360b48c6c71397b24
Hojin Kim <miu.null@gmail.com>
2023-09-11 13:14:53 +0900

    example of number and string Enums

---
+++ src/others/numberEnum.ts
@@ -0,0 +1,11 @@
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+const myColor = Color.Red;
+console.log(myColor); // 0
+
+const yourColor = Color.Blue;
+console.log(yourColor); // 2
---
+++ src/others/stringEnum.ts
@@ -0,0 +1,13 @@
+import {Color} from "../types";
+
+const myColor = Color.Red;
+
+console.log(myColor);
+
+const midori: Color = Color.Green;
+console.log(midori);
+
+console.log("-----------------");
+const faveColor: Color = "Red" as Color;
+
+console.log(faveColor);
