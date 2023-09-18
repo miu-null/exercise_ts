52f88052bb29d0cfd833614da3d0c0033c29ed64
Hojin Kim <miu.null@gmail.com>
2023-09-18 14:35:33 +0900

    edit import

---
+++ src/Enums/enumTips.ts
@@ -0,0 +1,10 @@
+import {Color} from "../types";
+
+const {Red, Green, Blue: skyBlue} = Color;
+
+const green = Green;
+const veggies = Green;
+const pepper = Red;
+const sky = skyBlue;
+
+//DRY (Don't Repeat Yourself!)
