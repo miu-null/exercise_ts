c61710ec8fd851a941022a133b8974e710e52891
Hojin Kim <miu.null@gmail.com>
2023-09-22 20:38:36 +0900

    define generic types, add stack and queue

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,42 @@ export interface thingsInLife {
     colorOfChair: Color;
     arrow: Direction;
 }
+
+export interface MyInterface {
+    value: string | number | Array<string>;
+}
+
+export interface MyInterfaceG<T = string> {
+    value: T;
+}
+
+export type User = {
+    email: string;
+    name: string;
+};
+
+export enum Status {
+    Initiated = "Initiated",
+    Pending = "Pending",
+    Shipped = "Shipped",
+    Delivered = "Delivered",
+}
+
+export interface Order {
+    buyer: string;
+    orderStatus: Status;
+}
+
+export interface IStack<T> {
+    push(item: T): void;
+    pop(): T | undefined;
+    peek(): T | undefined;
+    size(): number;
+}
+
+export interface IQueue<T> {
+    enqueue(item: T): number;
+    dequeue(): T | undefined;
+    peek(): T | undefined;
+    isEmpty(): boolean;
+}
